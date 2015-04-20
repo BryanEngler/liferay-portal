cc1b52157e8f2b9bbf6d70c8d78d58c783d08bcb
arbetts <andrew.betts@liferay.com>
2015-04-20 16:22:52 -0700

    LPS-54744 move logic from publish_process_message_task_details to staging background task display object

--- portal-impl/src/com/liferay/portal/staging/StagingImpl.java
+++ portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -599,23 +599,23 @@ public class StagingImpl implements Staging {
 						true));
 			}
 
-			errorMessageJSONObject.put("name", missingReferenceDisplayName);
+			errorMessageJSONObject.put(
+				"itemStrongMessage", missingReferenceDisplayName);
 
 			Group group = GroupLocalServiceUtil.fetchGroup(
 				missingReference.getGroupId());
 
+			String itemMessage = ResourceActionsUtil.getModelResource(
+				locale, missingReference.getClassName());
+
 			if (group != null) {
-				errorMessageJSONObject.put(
-					"site",
+				itemMessage += StringPool.SPACE +
 					LanguageUtil.format(
 						locale, "in-site-x", missingReference.getGroupId(),
-						false));
+						false);
 			}
 
-			errorMessageJSONObject.put(
-				"type",
-				ResourceActionsUtil.getModelResource(
-					locale, missingReference.getClassName()));
+			errorMessageJSONObject.put("itemMessage", itemMessage);
 
 			errorMessagesJSONArray.put(errorMessageJSONObject);
 		}
@@ -738,13 +738,14 @@ public class StagingImpl implements Staging {
 				String layoutPrototypeName =
 					(String)missingLayoutPrototype.getObject(2);
 
-				errorMessageJSONObject.put("name", layoutPrototypeName);
+				errorMessageJSONObject.put(
+					"itemStrongMessage", layoutPrototypeName);
 
 				String layoutPrototypeClassName =
 					(String)missingLayoutPrototype.getObject(0);
 
 				errorMessageJSONObject.put(
-					"type",
+					"itemMessage",
 					ResourceActionsUtil.getModelResource(
 						locale, layoutPrototypeClassName));
 
@@ -1068,9 +1069,9 @@ public class StagingImpl implements Staging {
 						false));
 			}
 
-			errorMessageJSONObject.put("size", referrers.size());
+			errorMessageJSONObject.put("itemStrongMessage", referrers.size());
 			errorMessageJSONObject.put(
-				"type",
+				"itemMessage",
 				ResourceActionsUtil.getModelResource(
 					locale, missingReferenceReferrerClassName));
 
