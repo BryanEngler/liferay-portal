09f6fd68b6642b25ac48fcbcb9f4ae3bb10259ef
Kevin Cruz <kevin.cruz@liferay.com>
2015-06-16 15:42:52 -0700

    Added commands to LocalProcessExecutor.java to supress unnecessary warnings

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -86,6 +86,7 @@ import com.liferay.util.EncryptorException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
