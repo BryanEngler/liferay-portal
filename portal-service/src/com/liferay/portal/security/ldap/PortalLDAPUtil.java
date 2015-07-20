7cc9a054d724379fb04b6dfcf94ccaa3c94c7355
ChrisKian <Christopher.Kian@liferay.com>
2015-07-20 14:21:10 -0700

    LPS-57240 Disable password reset if LDAP user

--- portal-service/src/com/liferay/portal/security/ldap/PortalLDAPUtil.java
+++ portal-service/src/com/liferay/portal/security/ldap/PortalLDAPUtil.java
@@ -31,6 +31,17 @@ import javax.naming.ldap.LdapContext;
  */
 public class PortalLDAPUtil {
 
+	public static LdapContext fetchContext(long ldapServerId, long companyId)
+		throws Exception {
+
+		try {
+			return PortalLDAPUtil.getContext(ldapServerId, companyId);
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
 	public static LdapContext getContext(long ldapServerId, long companyId)
 		throws Exception {
 
