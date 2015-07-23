a01a66866e905e382d73cde8311326d20db1e516
Steven Smith <steven.smith@liferay.com>
2015-07-23 14:09:55 -0700

    LPS-57318 Do not need to imply which month to start monthly publish, otherwise we start only on that month, each year

--- portal-service/src/com/liferay/portal/kernel/cal/RecurrenceSerializer.java
+++ portal-service/src/com/liferay/portal/kernel/cal/RecurrenceSerializer.java
@@ -91,7 +91,7 @@ public class RecurrenceSerializer {
 		}
 		else if (frequency == Recurrence.MONTHLY) {
 			dayOfMonth = StringPool.QUESTION;
-			month += StringPool.FORWARD_SLASH + interval;
+			month = StringPool.STAR;
 			dayOfWeek = StringPool.QUESTION;
 			year = StringPool.STAR;
 
