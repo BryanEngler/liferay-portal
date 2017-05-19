8d2aba428e2f336d8860f36195aecc7d1b054307
Bryan Engler <bryan.engler@liferay.com>
2017-05-19 16:26:20 -0700

    LPS-72703 Add permission filter to search query
    https://issues.liferay.com/browse/LPS-72703

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -1585,6 +1585,19 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 		return getClassName();
 	}
 
+	protected List<SearchQueryPermissionFilterClassNameContributer>
+		getContributors() {
+
+		if (_contributors != null) {
+			return _contributors;
+		}
+
+		_contributors = ServiceTrackerCollections.openList(
+			SearchQueryPermissionFilterClassNameContributer.class);
+
+		return _contributors;
+	}
+
 	protected String[] getDefaultSelectedFieldNames() {
 		return _defaultSelectedFieldNames;
 	}
@@ -1877,6 +1890,7 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 	private static final Log _log = LogFactoryUtil.getLog(BaseIndexer.class);
 
 	private boolean _commitImmediately;
+	private List<SearchQueryPermissionFilterClassNameContributer> _contributors;
 	private String[] _defaultSelectedFieldNames;
 	private String[] _defaultSelectedLocalizedFieldNames;
 	private final Document _document = new DocumentImpl();
