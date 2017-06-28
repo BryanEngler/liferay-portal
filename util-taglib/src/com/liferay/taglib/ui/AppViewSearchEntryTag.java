1a08edd4a8451d4c8cb74b5bf954f3f2e3f23b5d
Bryan Engler <bryan.engler@liferay.com>
2017-06-28 11:51:19 -0700

    LPS-72670 util-taglib: Add highlighted description and title to app-view-search-entry
    https://issues.liferay.com/browse/LPS-72670

--- util-taglib/src/com/liferay/taglib/ui/AppViewSearchEntryTag.java
+++ util-taglib/src/com/liferay/taglib/ui/AppViewSearchEntryTag.java
@@ -77,6 +77,14 @@ public class AppViewSearchEntryTag extends IncludeTag {
 		_fileEntryRelatedSearchResults = fileEntryRelatedSearchResults;
 	}
 
+	public void setHighlightedDescription(String highlightedDescription) {
+		_highlightedDescription = highlightedDescription;
+	}
+
+	public void setHighlightedTitle(String highlightedTitle) {
+		_highlightedTitle = highlightedTitle;
+	}
+
 	public void setHighlightEnabled(boolean highlightEnabled) {
 		_highlightEnabled = highlightEnabled;
 	}
@@ -135,6 +143,8 @@ public class AppViewSearchEntryTag extends IncludeTag {
 		_description = null;
 		_escape = true;
 		_fileEntryRelatedSearchResults = null;
+		_highlightedDescription = null;
+		_highlightedTitle = null;
 		_highlightEnabled = false;
 		_locked = false;
 		_rowCheckerId = null;
@@ -189,6 +199,12 @@ public class AppViewSearchEntryTag extends IncludeTag {
 			"liferay-ui:app-view-search-entry:fileEntryRelatedSearchResults",
 			_fileEntryRelatedSearchResults);
 		request.setAttribute(
+			"liferay-ui:app-view-search-entry:highlightedDescription",
+			_highlightedDescription);
+		request.setAttribute(
+			"liferay-ui:app-view-search-entry:highlightedTitle",
+			_highlightedTitle);
+		request.setAttribute(
 			"liferay-ui:app-view-search-entry:highlightEnabled",
 			_highlightEnabled);
 		request.setAttribute(
@@ -223,6 +239,8 @@ public class AppViewSearchEntryTag extends IncludeTag {
 	private String _description;
 	private boolean _escape = true;
 	private List<RelatedSearchResult<FileEntry>> _fileEntryRelatedSearchResults;
+	private String _highlightedDescription;
+	private String _highlightedTitle;
 	private boolean _highlightEnabled;
 	private boolean _locked;
 
