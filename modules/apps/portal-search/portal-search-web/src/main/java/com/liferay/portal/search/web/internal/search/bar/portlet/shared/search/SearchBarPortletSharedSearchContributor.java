4c760768d08e3185d41a5685898e18cdb43b805f
Bryan Engler <bryan.engler@liferay.com>
2017-08-30 13:07:28 -0700

    LPS-74551 portal-search-web: Properly set searchContext.groupIds for pre-filter. Classic search uses searchContext.attribute("groupId") for post filter, so dont allow "0" which can be there by default
    https://issues.liferay.com/browse/LPS-74551

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
@@ -14,10 +14,7 @@
 
 package com.liferay.portal.search.web.internal.search.bar.portlet.shared.search;
 
-import com.liferay.portal.kernel.search.BooleanClauseOccur;
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.generic.BooleanClauseImpl;
-import com.liferay.portal.kernel.search.generic.TermQueryImpl;
+import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.search.web.internal.display.context.SearchScope;
 import com.liferay.portal.search.web.internal.display.context.SearchScopePreference;
@@ -62,13 +59,12 @@ public class SearchBarPortletSharedSearchContributor
 		Optional<Long> groupIdOptional = getThisSiteGroupId(
 			searchBarPortletPreferences, portletSharedSearchSettings);
 
+		SearchContext searchContext =
+			portletSharedSearchSettings.getSearchContext();
+
 		groupIdOptional.ifPresent(
 			groupId -> {
-				portletSharedSearchSettings.addCondition(
-					new BooleanClauseImpl(
-						new TermQueryImpl(
-							Field.GROUP_ID, String.valueOf(groupId)),
-						BooleanClauseOccur.MUST));
+				searchContext.setGroupIds(new long[] {groupId});
 			});
 	}
 
