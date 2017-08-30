601e14e5529b83c83ec74090833364f357662a0d
Bryan Engler <bryan.engler@liferay.com>
2017-08-30 12:45:30 -0700

    LPS-74555 portal-search-elasticsearch: Update facet processors to call new method
    https://issues.liferay.com/browse/LPS-74555

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/facet/DefaultFacetProcessor.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/facet/DefaultFacetProcessor.java
@@ -17,9 +17,13 @@ package com.liferay.portal.search.elasticsearch.internal.facet;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.search.facet.Facet;
 import com.liferay.portal.kernel.search.facet.config.FacetConfiguration;
-import com.liferay.portal.search.elasticsearch.facet.FacetProcessor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 
 import org.osgi.service.component.annotations.Component;
@@ -29,12 +33,19 @@ import org.osgi.service.component.annotations.Component;
  * @author Milen Dyankov
  */
 @Component(immediate = true, property = {"class.name=DEFAULT"})
-public class DefaultFacetProcessor
-	implements FacetProcessor<SearchRequestBuilder> {
+public class DefaultFacetProcessor extends BaseFacetProcessor {
 
 	@Override
 	public void processFacet(
 		SearchRequestBuilder searchRequestBuilder, Facet facet) {
+
+		processFacet(searchRequestBuilder, facet, new HashMap<>());
+	}
+
+	@Override
+	public void processFacet(
+		SearchRequestBuilder searchRequestBuilder, Facet facet,
+		Map<String, List<QueryBuilder>> filterAggregationQueryBuildersMap) {
 
 		FacetConfiguration facetConfiguration = facet.getFacetConfiguration();
 
@@ -58,7 +69,15 @@ public class DefaultFacetProcessor
 			termsBuilder.size(size);
 		}
 
-		searchRequestBuilder.addAggregation(termsBuilder);
+		if (filterAggregationQueryBuildersMap.isEmpty()) {
+			searchRequestBuilder.addAggregation(termsBuilder);
+
+			return;
+		}
+
+		addFilteredAggregations(
+			searchRequestBuilder, fieldName, termsBuilder,
+			filterAggregationQueryBuildersMap);
 	}
 
 }
