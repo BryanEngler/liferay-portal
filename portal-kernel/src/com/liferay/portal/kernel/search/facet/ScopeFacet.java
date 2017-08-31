916646b549f8047ecc54b65a948156aebb6dfd53
Bryan Engler <bryan.engler@liferay.com>
2017-08-31 12:22:58 -0700

    LPS-72998 portal-kernel: Use "facetSelections" in the facet configuration to store post-filter selections because the old search portlet already uses "values" in the facet configuration to store pre-filter selections
    https://issues.liferay.com/browse/LPS-72998

--- portal-kernel/src/com/liferay/portal/kernel/search/facet/ScopeFacet.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/ScopeFacet.java
@@ -167,16 +167,17 @@ public class ScopeFacet extends MultiValueFacet {
 
 		JSONObject dataJSONObject = facetConfiguration.getData();
 
-		if (!dataJSONObject.has("values")) {
+		if (!dataJSONObject.has("facetSelections")) {
 			return null;
 		}
 
-		JSONArray valuesJSONArray = dataJSONObject.getJSONArray("values");
+		JSONArray facetSelectionsJSONArray = dataJSONObject.getJSONArray(
+			"facetSelections");
 
-		long[] groupIds = new long[valuesJSONArray.length()];
+		long[] groupIds = new long[facetSelectionsJSONArray.length()];
 
-		for (int i = 0; i < valuesJSONArray.length(); i++) {
-			groupIds[i] = valuesJSONArray.getLong(i);
+		for (int i = 0; i < facetSelectionsJSONArray.length(); i++) {
+			groupIds[i] = facetSelectionsJSONArray.getLong(i);
 		}
 
 		return groupIds;
