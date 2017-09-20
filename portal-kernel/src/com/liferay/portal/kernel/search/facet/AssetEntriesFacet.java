50b7ffb6f6f34dd91e2e362160fe0e9764eaa9e0
Bryan Engler <bryan.engler@liferay.com>
2017-09-20 09:48:42 -0700

    Revert "LPS-72998 portal-kernel: deprecate AssetEntriesFacet"
    This reverts commit b155b863c563ee8cc87ae9ecfc52ea201d79447f.

--- portal-kernel/src/com/liferay/portal/kernel/search/facet/AssetEntriesFacet.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/AssetEntriesFacet.java
@@ -40,9 +40,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 
 /**
  * @author Raymond Augé
- * @deprecated As of 7.0.0, with no direct replacement
  */
-@Deprecated
 public class AssetEntriesFacet extends MultiValueFacet {
 
 	public AssetEntriesFacet(SearchContext searchContext) {
