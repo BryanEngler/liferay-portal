16bd11d2fc18eeb837c9cba70fcd891356a4a1de
Bryan Engler <bryan.engler@liferay.com>
2017-09-19 18:16:42 -0700

    LPS-74655 portal-search-facet: Reorder logic to set query, then pre-filter, then post-filter
    https://issues.liferay.com/browse/LPS-74655

--- modules/apps/foundation/portal-search/portal-search-facet/src/main/java/com/liferay/portal/search/facet/internal/faceted/searcher/FacetedSearcherImpl.java
+++ modules/apps/foundation/portal-search/portal-search-facet/src/main/java/com/liferay/portal/search/facet/internal/faceted/searcher/FacetedSearcherImpl.java
@@ -149,6 +149,8 @@ public class FacetedSearcherImpl
 	protected BooleanQuery createFullQuery(SearchContext searchContext)
 		throws Exception {
 
+		BooleanQuery fullQuery = new BooleanQueryImpl();
+
 		BooleanQuery searchQuery = new BooleanQueryImpl();
 
 		String keywords = searchContext.getKeywords();
@@ -171,12 +173,20 @@ public class FacetedSearcherImpl
 			searchQuery, fullQueryBooleanFilter, luceneSyntax,
 			entryClassNameIndexerMap, searchContext);
 
+		if (searchQuery.hasClauses()) {
+			fullQuery.add(searchQuery, BooleanClauseOccur.MUST);
+		}
+
 		_addPreFilters(
 			fullQueryBooleanFilter, entryClassNameIndexerMap, searchContext);
 
-		Map<String, Facet> facets = searchContext.getFacets();
+		if (fullQueryBooleanFilter.hasClauses()) {
+			fullQuery.setPreBooleanFilter(fullQueryBooleanFilter);
+		}
 
 		BooleanFilter facetBooleanFilter = new BooleanFilter();
+
+		Map<String, Facet> facets = searchContext.getFacets();
 
 		for (Facet facet : facets.values()) {
 			BooleanClause<Filter> facetClause =
@@ -191,18 +201,8 @@ public class FacetedSearcherImpl
 
 		addFacetClause(searchContext, facetBooleanFilter, facets.values());
 
-		BooleanQuery fullQuery = new BooleanQueryImpl();
-
 		if (facetBooleanFilter.hasClauses()) {
 			fullQuery.setPostFilter(facetBooleanFilter);
-		}
-
-		if (fullQueryBooleanFilter.hasClauses()) {
-			fullQuery.setPreBooleanFilter(fullQueryBooleanFilter);
-		}
-
-		if (searchQuery.hasClauses()) {
-			fullQuery.add(searchQuery, BooleanClauseOccur.MUST);
 		}
 
 		BooleanClause<Query>[] booleanClauses =
