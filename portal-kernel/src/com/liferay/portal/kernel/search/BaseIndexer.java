09d6bef9ccbbb71385709d3a679f88711ba7a803
Bryan Engler <bryan.engler@liferay.com>
2017-09-21 17:10:55 -0700

    LPS-74655 portal-kernel: Use scopeGroupId to keep track of the site the user is searching from
    https://issues.liferay.com/browse/LPS-74655

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -2050,6 +2050,11 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 			searchContext.getAttribute(Field.GROUP_ID));
 
 		if (groupId == 0) {
+			groupId = GetterUtil.getLong(
+				searchContext.getAttribute(Field.SCOPE_GROUP_ID));
+		}
+
+		if (groupId == 0) {
 			return null;
 		}
 
