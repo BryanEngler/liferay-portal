a81d00423b27a3dc98cd44af8c9bde99e26def29
Bryan Engler <bryan.engler@liferay.com>
2017-11-02 17:51:08 -0700

    LPS-75734 portal-search-test-util: Add base class
    https://issues.liferay.com/browse/LPS-75734

---
+++ modules/apps/foundation/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/document/BaseSingleFieldFixture.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.test.util.document;
+
+import java.util.List;
+
+/**
+ * @author Bryan Engler
+ */
+public abstract class BaseSingleFieldFixture implements SingleFieldFixture {
+
+	@Override
+	public void deleteDocuments(List<String> uids) throws Exception {
+	}
+
+	@Override
+	public void setField(String field) {
+		_field = field;
+	}
+
+	protected String getField() {
+		return _field;
+	}
+
+	private String _field;
+
+}
