7cedcebf26296c5135141f69b3eee801644bbaa1
Bryan Engler <bryan.engler@liferay.com>
2017-11-02 17:50:40 -0700

    LPS-75734 portal-search-test-util: Add SingleFieldFixture interface
    https://issues.liferay.com/browse/LPS-75734

---
+++ modules/apps/foundation/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/document/SingleFieldFixture.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.test.util.document;
+
+import java.util.List;
+
+/**
+ * @author Bryan Engler
+ */
+public interface SingleFieldFixture {
+
+	public void assertHighlights(String text, String... expected)
+		throws Exception;
+
+	public void assertNoHits(String text) throws Exception;
+
+	public void assertSearch(String text, String... expected) throws Exception;
+
+	public void deleteDocuments(List<String> uids) throws Exception;
+
+	public String indexDocument(String value) throws Exception;
+
+	public void setField(String field);
+
+	public void setSingleFieldQueryFactory(
+		SingleFieldQueryFactory queryFactory);
+
+}
