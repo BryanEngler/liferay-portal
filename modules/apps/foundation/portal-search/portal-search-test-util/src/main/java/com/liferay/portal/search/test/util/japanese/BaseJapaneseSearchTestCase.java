6a18c9e2d089f03f175565032baf757894879e84
Bryan Engler <bryan.engler@liferay.com>
2017-11-02 18:37:22 -0700

    LPS-75734 portal-search-test-util: Add base test class
    https://issues.liferay.com/browse/LPS-75734

---
+++ modules/apps/foundation/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/japanese/BaseJapaneseSearchTestCase.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.test.util.japanese;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.search.test.util.document.SingleFieldFixture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+/**
+ * @author Bryan Engler
+ */
+public abstract class BaseJapaneseSearchTestCase {
+
+	@After
+	public void tearDown() throws Exception {
+		uids.clear();
+	}
+
+	@Test
+	public void testNoSpaceSentenceSearch1() throws Exception {
+		String content1 = "すももももももものうち";
+
+		index(content1);
+
+		assertSearch("すもも", content1);
+		assertSearch("もも", content1);
+
+		assertNoHits("も");
+	}
+
+	@Test
+	public void testNoSpaceSentenceSearch2() throws Exception {
+		String content1 = "東京特許許可局";
+
+		index(content1);
+
+		assertSearch("東京", content1);
+		assertSearch("特許", content1);
+		assertSearch("許可", content1);
+		assertSearch("許可局", content1);
+	}
+
+	@Test
+	public void testSearch1() throws Exception {
+		String content1 = "作戦大成功";
+		String content2 = "新規作戦";
+		String content3 = "映像製作会社";
+		String content4 = "作文を書いた";
+
+		index(content1, content2, content3, content4);
+
+		assertSearch("作戦", content1, content2);
+
+		assertSearch("製作", content3);
+
+		assertSearch("作文", content4);
+
+		assertNoHits("作");
+	}
+
+	@Test
+	public void testSearch2() throws Exception {
+		String content1 = "作戦大成功";
+		String content2 = "新規作戦";
+		String content3 = "映像製作会社";
+		String content4 = "作文を書いた";
+		String content5 = "新しい作戦";
+		String content6 = "新規作成";
+		String content7 = "新世界";
+		String content8 = "新着情報";
+
+		index(
+			content1, content2, content3, content4, content5, content6,
+			content7, content8);
+
+		assertSearch("作戦", content1, content2, content5);
+
+		assertSearch("新規", content2, content6);
+
+		assertSearch("新", content7);
+	}
+
+	@Test
+	public void testSearch3() throws Exception {
+		String content1 = "作戦大成功";
+		String content2 = "新規作戦";
+		String content3 = "映像製作会社";
+		String content4 = "作文を書いた";
+		String content5 = "新しい作戦";
+		String content6 = "新規作成";
+		String content7 = "新世界";
+		String content8 = "新着情報";
+		String content9 = "新規作戦";
+
+		index(
+			content1, content2, content3, content4, content5, content6,
+			content7, content8, content9);
+
+		assertSearch("新規", content2, content6, content9);
+
+		assertSearch("新", content7);
+	}
+
+	@Test
+	public void testSearch4() throws Exception {
+		String content1 = "作戦大成功";
+		String content2 = "新大阪";
+		String content3 = "新規作成";
+		String content4 = "東京特許許可局局長";
+		String content5 = "京都";
+
+		index(content1, content2, content3, content4, content5);
+
+		assertSearch("新規", content3);
+
+		assertSearch("作成", content3);
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
+	protected void assertNoHits(String query) throws Exception {
+		singleFieldFixture.assertNoHits(query);
+	}
+
+	protected void assertSearch(String query, String... expected)
+		throws Exception {
+
+		singleFieldFixture.assertSearch(query, expected);
+	}
+
+	protected void index(String... strings) {
+		try {
+			for (String string : strings) {
+				uids.add(singleFieldFixture.indexDocument(string));
+			}
+		}
+		catch (Exception e) {
+			_log.error("Unable to index documents", e);
+		}
+	}
+
+	protected SingleFieldFixture singleFieldFixture;
+	protected List<String> uids = new ArrayList<>();
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseJapaneseSearchTestCase.class);
+
+}
