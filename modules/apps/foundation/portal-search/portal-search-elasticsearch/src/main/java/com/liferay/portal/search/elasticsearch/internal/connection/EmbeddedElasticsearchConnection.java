a500d9084165109fe65ccea8b5ef53427d5591f2
André de Oliveira <andre.oliveira@liferay.com>
2017-11-28 21:03:43 -0800

    EmbeddedElasticsearchConnection number of replicas and shards make sure moved to index creation

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/EmbeddedElasticsearchConnection.java
@@ -417,9 +417,6 @@ public class EmbeddedElasticsearchConnection
 
 		configureHttp();
 
-		settingsBuilder.put("index.number_of_replicas", 0);
-		settingsBuilder.put("index.number_of_shards", 1);
-
 		configureNetworking();
 
 		settingsBuilder.put("node.data", true);
