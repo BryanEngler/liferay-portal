3a0e64e291d2b97b9fb587215f7d28f53f301520
André de Oliveira <andre.oliveira@liferay.com>
2018-02-03 18:23:06 -0800

    Go 6: RemoteElasticsearchConnection: Elasticsearch Java API changes

--- modules/apps/foundation/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
+++ modules/apps/foundation/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch/internal/connection/RemoteElasticsearchConnection.java
@@ -20,10 +20,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Props;
-import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.search.elasticsearch.configuration.ElasticsearchConfiguration;
 import com.liferay.portal.search.elasticsearch.connection.ElasticsearchConnection;
 import com.liferay.portal.search.elasticsearch.connection.OperationMode;
@@ -39,8 +37,8 @@ import java.util.Set;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -108,7 +106,7 @@ public class RemoteElasticsearchConnection extends BaseElasticsearchConnection {
 		InetAddress inetAddress = InetAddress.getByName(host);
 
 		transportClient.addTransportAddress(
-			new InetSocketTransportAddress(inetAddress, port));
+			new TransportAddress(inetAddress, port));
 	}
 
 	@Override
@@ -137,32 +135,12 @@ public class RemoteElasticsearchConnection extends BaseElasticsearchConnection {
 	}
 
 	protected TransportClient createTransportClient() {
-		TransportClient.Builder transportClientBuilder =
-			TransportClient.builder();
-
-		transportClientBuilder.settings(settingsBuilder);
-
-		for (String plugin : transportClientPlugins) {
-			transportClientBuilder.addPlugin(getPluginClass(plugin));
-		}
-
-		return transportClientBuilder.build();
+		return new PreBuiltTransportClient(settingsBuilder.build());
 	}
 
 	@Deactivate
 	protected void deactivate(Map<String, Object> properties) {
 		close();
-	}
-
-	@SuppressWarnings("unchecked")
-	protected Class<? extends Plugin> getPluginClass(String plugin) {
-		try {
-			return (Class<? extends Plugin>)Class.forName(plugin);
-		}
-		catch (ClassNotFoundException cnfe) {
-			throw new IllegalArgumentException(
-				"Elasticsearch plugin class not found: " + plugin, cnfe);
-		}
 	}
 
 	@Override
@@ -174,17 +152,13 @@ public class RemoteElasticsearchConnection extends BaseElasticsearchConnection {
 			"client.transport.nodes_sampler_interval",
 			elasticsearchConfiguration.clientTransportNodesSamplerInterval());
 		settingsBuilder.put(
+			"client.transport.ping_timeout",
+			elasticsearchConfiguration.clientTransportPingTimeout());
+		settingsBuilder.put(
 			"client.transport.sniff",
 			elasticsearchConfiguration.clientTransportSniff());
 		settingsBuilder.put(
 			"cluster.name", elasticsearchConfiguration.clusterName());
-		settingsBuilder.put("http.enabled", false);
-		settingsBuilder.put("node.client", true);
-		settingsBuilder.put("node.data", false);
-		settingsBuilder.put(
-			"path.logs", props.get(PropsKeys.LIFERAY_HOME) + "/logs");
-		settingsBuilder.put(
-			"path.work", SystemProperties.get(SystemProperties.TMP_DIR));
 		settingsBuilder.put(
 			"request.headers.X-Found-Cluster",
 			elasticsearchConfiguration.clusterName());
