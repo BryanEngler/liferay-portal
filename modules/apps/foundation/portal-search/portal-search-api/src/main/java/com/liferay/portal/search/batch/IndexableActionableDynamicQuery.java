fb22d6236f37b43119db5fe3b7a344a9cca5703e
Bryan Engler <bryan.engler@liferay.com>
2018-05-02 16:09:08 -0700

    LPS-73059 portal-search-api: Add IndexableActionableDynamicQuery and IndexableActionableDynamicQueryFactory interfaces
    https://issues.liferay.com/browse/LPS-73059

---
+++ modules/apps/foundation/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/batch/IndexableActionableDynamicQuery.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.batch;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Document;
+
+/**
+ * @author Bryan Engler
+ */
+@ProviderType
+public interface IndexableActionableDynamicQuery
+	extends ActionableDynamicQuery {
+
+	public void addDocuments(Document... documents) throws PortalException;
+
+	public void setSearchEngineId(String searchEngineId);
+
+}
