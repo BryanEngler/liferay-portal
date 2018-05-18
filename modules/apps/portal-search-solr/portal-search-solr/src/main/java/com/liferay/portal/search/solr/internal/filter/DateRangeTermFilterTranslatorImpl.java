62320d020724129e80d2e2d9a70c532b76b121f2
Eric Yan <eric.yan@liferay.com>
2018-05-18 11:43:27 -0700

    LPS-80931 portal-search-solr: account for date format and timezone

--- modules/apps/portal-search-solr/portal-search-solr/src/main/java/com/liferay/portal/search/solr/internal/filter/DateRangeTermFilterTranslatorImpl.java
+++ modules/apps/portal-search-solr/portal-search-solr/src/main/java/com/liferay/portal/search/solr/internal/filter/DateRangeTermFilterTranslatorImpl.java
@@ -15,12 +15,21 @@
 package com.liferay.portal.search.solr.internal.filter;
 
 import com.liferay.portal.kernel.search.filter.DateRangeTermFilter;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.portal.search.solr.filter.DateRangeTermFilterTranslator;
+import com.liferay.portal.search.solr.internal.util.ZonedDateTimeUtil;
+
+import java.util.TimeZone;
 
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermRangeQuery;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael C. Han
@@ -31,13 +40,58 @@ public class DateRangeTermFilterTranslatorImpl
 
 	@Override
 	public Query translate(DateRangeTermFilter dateRangeTermFilter) {
+		String dateFormat = dateRangeTermFilter.getDateFormat();
+		String lowerBound = dateRangeTermFilter.getLowerBound();
+		TimeZone timeZone = dateRangeTermFilter.getTimeZone();
+		String upperBound = dateRangeTermFilter.getUpperBound();
+
+		if (!_dateFormatPattern.equals(dateFormat) ||
+			!_TIME_ZONE_ID.equals(timeZone.getID())) {
+
+			String[] dateFormats = StringUtil.split(
+				dateFormat, _DATE_FORMAT_SEPARATOR);
+
+			try {
+				if (lowerBound != null) {
+					lowerBound = ZonedDateTimeUtil.formatDate(
+						dateFormats, _dateFormatPattern, lowerBound,
+						timeZone.toZoneId(), _TIME_ZONE.toZoneId());
+				}
+
+				if (upperBound != null) {
+					upperBound = ZonedDateTimeUtil.formatDate(
+						dateFormats, _dateFormatPattern, upperBound,
+						timeZone.toZoneId(), _TIME_ZONE.toZoneId());
+				}
+			}
+			catch (Exception e) {
+				throw new IllegalArgumentException(
+					"Invalid date range " + dateRangeTermFilter, e);
+			}
+		}
+
 		TermRangeQuery termRangeQuery = TermRangeQuery.newStringRange(
-			dateRangeTermFilter.getField(), dateRangeTermFilter.getLowerBound(),
-			dateRangeTermFilter.getUpperBound(),
+			dateRangeTermFilter.getField(), lowerBound, upperBound,
 			dateRangeTermFilter.isIncludesLower(),
 			dateRangeTermFilter.isIncludesUpper());
 
 		return termRangeQuery;
 	}
+
+	@Activate
+	protected void activate() {
+		_dateFormatPattern = props.get(PropsKeys.INDEX_DATE_FORMAT_PATTERN);
+	}
+
+	@Reference
+	protected Props props;
+
+	private static final String _DATE_FORMAT_SEPARATOR = "||";
+
+	private static final TimeZone _TIME_ZONE = TimeZoneUtil.getDefault();
+
+	private static final String _TIME_ZONE_ID = _TIME_ZONE.getID();
+
+	private String _dateFormatPattern;
 
 }
