5376159d4d206654facb963c41d0089f4f787118
Bryan Engler <bryan.engler@liferay.com>
2018-07-20 00:59:56 -0700

    SEARCH-3 portal-search-solr: GroupByTest

--- modules/apps/portal-search-solr/portal-search-solr/src/main/java/com/liferay/portal/search/solr/internal/SolrIndexSearcher.java
+++ modules/apps/portal-search-solr/portal-search-solr/src/main/java/com/liferay/portal/search/solr/internal/SolrIndexSearcher.java
@@ -741,6 +741,8 @@ public class SolrIndexSearcher extends BaseIndexSearcher {
 		Query query, Hits hits, List<Document> documents, List<Float> scores) {
 
 		if (solrDocumentList == null) {
+			documents.addAll(ListUtil.fromArray(hits.getDocs()));
+
 			return;
 		}
 
@@ -853,6 +855,8 @@ public class SolrIndexSearcher extends BaseIndexSearcher {
 					queryResponse, group.getResult(), query, groupedHits);
 
 				hits.addGroupedHits(group.getGroupValue(), groupedHits);
+
+				hits.setDocs(groupedHits.getDocs());
 			}
 		}
 	}
