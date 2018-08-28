aa1dcf5ce604b5932f923a7a3a918a9a160bde42
Bryan Engler <bryan.engler@liferay.com>
2018-08-28 12:57:52 -0700

    LPS-84837 portal-search-solr: Sort
    https://issues.liferay.com/browse/LPS-84837

--- modules/apps/portal-search-solr/portal-search-solr/src/test/java/com/liferay/portal/search/solr/internal/mappings/DescriptionFieldQueryBuilderTest.java
+++ modules/apps/portal-search-solr/portal-search-solr/src/test/java/com/liferay/portal/search/solr/internal/mappings/DescriptionFieldQueryBuilderTest.java
@@ -35,12 +35,19 @@ public class DescriptionFieldQueryBuilderTest
 		addDocument("Tabs Names Tags");
 
 		assertSearch("\"name ta*\"", 1);
+		assertSearch("\"name tab*\"", 1);
+		assertSearch("\"name tabs*\"", 1);
 		assertSearch("\"name tag*\"", 1);
 		assertSearch("\"name tags*\"", 1);
 		assertSearch("\"names ta*\"", 3);
 		assertSearch("\"names tab*\"", 3);
+		assertSearch("\"names tabs*\"", 3);
 		assertSearch("\"names tag*\"", 3);
 		assertSearch("\"names tags*\"", 3);
+		assertSearch("\"tab na*\"", 1);
+		assertSearch("\"tab names*\"", 1);
+		assertSearch("\"tabs na ta*\"", 1);
+		assertSearch("\"tabs name ta*\"", 1);
 		assertSearch("\"tabs name*\"", 1);
 		assertSearch("\"tabs names ta*\"", 1);
 		assertSearch("\"tabs names tag*\"", 1);
@@ -49,17 +56,10 @@ public class DescriptionFieldQueryBuilderTest
 		assertSearch("\"tag na*\"", 1);
 		assertSearch("\"tag name*\"", 1);
 		assertSearch("\"tag names*\"", 1);
-
-		assertSearch("\"name tab*\"", 1);
-		assertSearch("\"name tabs*\"", 1);
-		assertSearch("\"names tabs*\"", 3);
-		assertSearch("\"tab na*\"", 1);
-		assertSearch("\"tab names*\"", 1);
-		assertSearch("\"tabs na ta*\"", 1);
-		assertSearch("\"tabs name ta*\"", 1);
 		assertSearch("\"tags na ta*\"", 2);
 		assertSearch("\"tags names tabs*\"", 2);
 		assertSearch("\"tags names*\"", 2);
+
 		assertSearchNoHits("\"zz na*\"");
 		assertSearchNoHits("\"zz name*\"");
 		assertSearchNoHits("\"zz names*\"");
