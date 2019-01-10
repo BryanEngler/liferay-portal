9dff204d2a80325cfc2af7ccd8be76dc7b94fbf1
Bryan Engler <bryan.engler@liferay.com>
2019-01-10 11:59:39 -0800

    LPS-89072 portal-search-solr7-impl: Add translator interface
    https://issues.liferay.com/browse/LPS-89072

---
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/response/SearchResponseTranslator.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr7.internal.search.response;
+
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+
+import org.apache.solr.client.solrj.response.QueryResponse;
+
+/**
+ * @author Bryan Engler
+ */
+public interface SearchResponseTranslator {
+
+	public Hits translate(
+		QueryResponse queryResponse, SearchSearchRequest searchSearchRequest);
+
+}
