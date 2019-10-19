c4f0d36b9a0163b379582d2d1490ba9194eea9bc
Bryan Engler <bryan.engler@liferay.com>
2019-10-19 16:28:51 -0700

    LPS-102500 portal-search-elasticsearch7-api: Sort embedded configs
    https://issues.liferay.com/browse/LPS-102500

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
@@ -136,6 +136,7 @@ public interface ElasticsearchConfiguration {
 	)
 	public String restClientLoggerLevel();
 
+	//embedded
 	@Meta.AD(
 		deflt = "LiferayElasticsearchCluster",
 		description = "cluster-name-help", name = "cluster-name",
@@ -150,6 +151,12 @@ public interface ElasticsearchConfiguration {
 	public boolean bootstrapMlockAll();
 
 	@Meta.AD(
+		deflt = "9201", description = "embedded-http-port-help",
+		name = "embedded-http-port", required = false
+	)
+	public int embeddedHttpPort();
+
+	@Meta.AD(
 		deflt = "9300-9400",
 		description = "discovery-zen-ping-unicast-hosts-port-help",
 		name = "discovery-zen-ping-unicast-hosts-port", required = false
@@ -161,12 +168,6 @@ public interface ElasticsearchConfiguration {
 		required = false
 	)
 	public String networkHost();
-
-	@Meta.AD(
-		deflt = "9201", description = "embedded-http-port-help",
-		name = "embedded-http-port", required = false
-	)
-	public int embeddedHttpPort();
 
 	@Meta.AD(
 		deflt = "", description = "network-bind-host-help",
@@ -187,6 +188,12 @@ public interface ElasticsearchConfiguration {
 	public String transportTcpPort();
 
 	@Meta.AD(
+		description = "additional-configurations-help",
+		name = "additional-configurations", required = false
+	)
+	public String additionalConfigurations();
+
+	@Meta.AD(
 		deflt = "true", description = "http-cors-enabled-help",
 		name = "http-cors-enabled", required = false
 	)
@@ -204,11 +211,5 @@ public interface ElasticsearchConfiguration {
 		name = "http-cors-configurations", required = false
 	)
 	public String httpCORSConfigurations();
-
-	@Meta.AD(
-		description = "additional-configurations-help",
-		name = "additional-configurations", required = false
-	)
-	public String additionalConfigurations();
 
 }
