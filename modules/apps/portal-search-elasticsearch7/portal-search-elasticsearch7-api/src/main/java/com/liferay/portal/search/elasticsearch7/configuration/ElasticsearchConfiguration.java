4552b1d95da180f3f386a42cf8c37aaba5358300
Bryan Engler <bryan.engler@liferay.com>
2019-10-19 16:17:42 -0700

    LPS-102500 portal-search-elasticsearch7-api: Sort index configs
    https://issues.liferay.com/browse/LPS-102500

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-api/src/main/java/com/liferay/portal/search/elasticsearch7/configuration/ElasticsearchConfiguration.java
@@ -43,6 +43,43 @@ public interface ElasticsearchConfiguration {
 	)
 	public String[] networkHostAddresses();
 
+	//index settings
+	@Meta.AD(
+		deflt = "liferay-", description = "index-name-prefix-help",
+		name = "index-name-prefix", required = false
+	)
+	public String indexNamePrefix();
+
+	@Meta.AD(
+		deflt = "", description = "number-of-index-replicas-help",
+		name = "number-of-index-replicas", required = false
+	)
+	public String indexNumberOfReplicas();
+
+	@Meta.AD(
+		deflt = "", description = "number-of-index-shards-help",
+		name = "number-of-index-shards", required = false
+	)
+	public String indexNumberOfShards();
+
+	@Meta.AD(
+		description = "additional-index-configurations-help",
+		name = "additional-index-configurations", required = false
+	)
+	public String additionalIndexConfigurations();
+
+	@Meta.AD(
+		description = "additional-type-mappings-help",
+		name = "additional-type-mappings", required = false
+	)
+	public String additionalTypeMappings();
+
+	@Meta.AD(
+		description = "override-type-mappings-help",
+		name = "override-type-mappings", required = false
+	)
+	public String overrideTypeMappings();
+
 	@Meta.AD(
 		deflt = "false", description = "authentication-enabled-help",
 		name = "authentication-enabled", required = false
@@ -91,24 +128,6 @@ public interface ElasticsearchConfiguration {
 		required = false
 	)
 	public String clusterName();
-
-	@Meta.AD(
-		deflt = "liferay-", description = "index-name-prefix-help",
-		name = "index-name-prefix", required = false
-	)
-	public String indexNamePrefix();
-
-	@Meta.AD(
-		deflt = "", description = "number-of-index-replicas-help",
-		name = "number-of-index-replicas", required = false
-	)
-	public String indexNumberOfReplicas();
-
-	@Meta.AD(
-		deflt = "", description = "number-of-index-shards-help",
-		name = "number-of-index-shards", required = false
-	)
-	public String indexNumberOfShards();
 
 	@Meta.AD(
 		deflt = "false", description = "bootstrap-mlockall-help",
@@ -189,23 +208,5 @@ public interface ElasticsearchConfiguration {
 		name = "additional-configurations", required = false
 	)
 	public String additionalConfigurations();
-
-	@Meta.AD(
-		description = "additional-index-configurations-help",
-		name = "additional-index-configurations", required = false
-	)
-	public String additionalIndexConfigurations();
-
-	@Meta.AD(
-		description = "additional-type-mappings-help",
-		name = "additional-type-mappings", required = false
-	)
-	public String additionalTypeMappings();
-
-	@Meta.AD(
-		description = "override-type-mappings-help",
-		name = "override-type-mappings", required = false
-	)
-	public String overrideTypeMappings();
 
 }
