580065c9c404e14279f78abf9f6334816d1e9058
tina.tian <tina.tian@liferay.com>
2020-01-06 11:14:29 -0800

    LPS-104437 portal-search-elasticsearch7: let ElasticsearchConnectionManager connect/close remote connection

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchConnectionManager.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchConnectionManager.java
@@ -145,6 +145,28 @@ public class ElasticsearchConnectionManager
 	}
 
 	public void setOperationMode(OperationMode operationMode) {
+		if (operationMode == _operationMode) {
+			return;
+		}
+
+		for (Map.Entry<String, ElasticsearchConnection> entry :
+				_elasticsearchConnections.entrySet()) {
+
+			ElasticsearchConnection elasticsearchConnection = entry.getValue();
+
+			if (!Objects.equals(
+					EmbeddedElasticsearchConnection.CONNECTION_ID,
+					entry.getKey())) {
+
+				if (operationMode == OperationMode.REMOTE) {
+					elasticsearchConnection.connect();
+				}
+				else {
+					elasticsearchConnection.close();
+				}
+			}
+		}
+
 		_operationMode = operationMode;
 	}
 
@@ -157,6 +179,10 @@ public class ElasticsearchConnectionManager
 	)
 	public void setRemoteElasticsearchConnection(
 		ElasticsearchConnection elasticsearchConnection) {
+
+		if (_operationMode == OperationMode.REMOTE) {
+			elasticsearchConnection.connect();
+		}
 
 		ElasticsearchConnection oldElasticsearchConnection =
 			_elasticsearchConnections.put(
