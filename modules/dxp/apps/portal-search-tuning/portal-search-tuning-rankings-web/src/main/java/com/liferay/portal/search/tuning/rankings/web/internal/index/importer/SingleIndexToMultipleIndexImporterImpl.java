4fed4abf83ab30d35f5c59fdb9e20507133aea75
Bryan Engler <bryan.engler@liferay.com>
2020-09-18 08:10:32 -0700

    Revert "LPS-110124 portal-search-tuning-rankings-web: Log exception instead of throwing error."
    This reverts commit 535d68c6572080529747d15087e9a8eec154012c.

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/importer/SingleIndexToMultipleIndexImporterImpl.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/importer/SingleIndexToMultipleIndexImporterImpl.java
@@ -15,8 +15,6 @@
 package com.liferay.portal.search.tuning.rankings.web.internal.index.importer;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.CompanyService;
 import com.liferay.portal.search.document.Document;
@@ -110,20 +108,6 @@ public class SingleIndexToMultipleIndexImporterImpl
 	protected void createRankingIndices() {
 		List<Company> companies = _companyService.getCompanies();
 
-		try {
-			createRankingIndices(companies);
-		}
-		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to create result ranking indices for existing " +
-						"companies",
-					exception);
-			}
-		}
-	}
-
-	protected void createRankingIndices(List<Company> companies) {
 		Stream<Company> stream = companies.stream();
 
 		stream.map(
@@ -198,9 +182,6 @@ public class SingleIndexToMultipleIndexImporterImpl
 
 	protected static final RankingIndexName SINGLE_INDEX_NAME =
 		() -> "liferay-search-tuning-rankings";
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SingleIndexToMultipleIndexImporterImpl.class);
 
 	@Reference
 	private CompanyService _companyService;
