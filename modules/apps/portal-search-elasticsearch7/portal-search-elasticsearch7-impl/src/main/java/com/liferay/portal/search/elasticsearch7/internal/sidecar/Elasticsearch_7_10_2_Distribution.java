0f96bd9e77f07c3abfdf50321247b5d7f50eddf0
Bryan Engler <bryan.engler@liferay.com>
2021-09-22 14:34:22 -0700

    LPS-139636 portal-search-elasticsearch7-impl: Download artifacts from our nexus instead of elastic
    https://issues.liferay.com/browse/LPS-139636

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Elasticsearch_7_10_2_Distribution.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Elasticsearch_7_10_2_Distribution.java
@@ -25,50 +25,60 @@ public class Elasticsearch_7_10_2_Distribution implements Distribution {
 	@Override
 	public Distributable getElasticsearchDistributable() {
 		return new DistributableImpl(
-			"https://artifacts.elastic.co/downloads/elasticsearch" +
-				"/elasticsearch-oss-7.10.2-no-jdk-linux-x86_64.tar.gz",
-			_ELASTICSEARCH_CHECKSUM);
+			_ELASTICSEARCH_URL, _ELASTICSEARCH_CHECKSUM);
 	}
 
 	@Override
 	public List<Distributable> getPluginDistributables() {
 		return Arrays.asList(
-			new DistributableImpl(
-				"https://artifacts.elastic.co/downloads/elasticsearch-plugins" +
-					"/analysis-icu/analysis-icu-7.10.2.zip",
-				_ICU_CHECKSUM),
-			new DistributableImpl(
-				"https://artifacts.elastic.co/downloads/elasticsearch-plugins" +
-					"/analysis-kuromoji/analysis-kuromoji-7.10.2.zip",
-				_KUROMOJI_CHECKSUM),
-			new DistributableImpl(
-				"https://artifacts.elastic.co/downloads/elasticsearch-plugins" +
-					"/analysis-smartcn/analysis-smartcn-7.10.2.zip",
-				_SMARTCN_CHECKSUM),
-			new DistributableImpl(
-				"https://artifacts.elastic.co/downloads/elasticsearch-plugins" +
-					"/analysis-stempel/analysis-stempel-7.10.2.zip",
-				_STEMPEL_CHECKSUM));
+			new DistributableImpl(_ICU_URL, _ICU_CHECKSUM),
+			new DistributableImpl(_KUROMOJI_URL, _KUROMOJI_CHECKSUM),
+			new DistributableImpl(_SMARTCN_URL, _SMARTCN_CHECKSUM),
+			new DistributableImpl(_STEMPEL_URL, _STEMPEL_CHECKSUM));
 	}
+
+	private static final String _BASE_URL =
+		"https://repository.liferay.com/nexus/service/local/repositories" +
+			"/third-party/content/com/liferay/";
 
 	private static final String _ELASTICSEARCH_CHECKSUM =
 		"7b63237996569ccdc7c9d9e7cc097fcb23865396eddac30e5f02543484220d2fc70a" +
 			"7285b430877e5e76a5d8716d9682de9fc40d5e57a08f331e82011fc59756";
 
+	private static final String _ELASTICSEARCH_URL =
+		_BASE_URL + "org.elasticsearch.server/7.10.2" +
+			"/org.elasticsearch.server-7.10.2.tar.gz";
+
 	private static final String _ICU_CHECKSUM =
 		"ae2c92baf202d50a652b544b6281086749c871ac98fa581e4a9869063d1ff96df62d" +
 			"db501838ee10025a288fb6125b070f599c0e738a8db711b8beb62a4683d9";
+
+	private static final String _ICU_URL =
+		_BASE_URL + "org.elasticsearch.plugin.analysis.icu/7.10.2" +
+			"/org.elasticsearch.plugin.analysis.icu-7.10.2.zip";
 
 	private static final String _KUROMOJI_CHECKSUM =
 		"3bd9296a4b00f76974e2d45ddd6de79113a6a2d42b05c0fb8df2f9ee8264a93709de" +
 			"fec66a16d6dea5dd40b8ea719827aa80be0afec4eee80bfca1bfee6fb33f";
 
+	private static final String _KUROMOJI_URL =
+		_BASE_URL + "org.elasticsearch.plugin.analysis.kuromoji/7.10.2" +
+			"/org.elasticsearch.plugin.analysis.kuromoji-7.10.2.zip";
+
 	private static final String _SMARTCN_CHECKSUM =
 		"d8e62d60178606f3148d86f46e90e9375808299f6ef945eea5d3df814f13658203ec" +
 			"6c20376c1583cb133840f05c9d38a5cd3af811cd64a356ce81a2c18a9c6a";
 
+	private static final String _SMARTCN_URL =
+		_BASE_URL + "org.elasticsearch.plugin.analysis.smartcn/7.10.2" +
+			"/org.elasticsearch.plugin.analysis.smartcn-7.10.2.zip";
+
 	private static final String _STEMPEL_CHECKSUM =
 		"5dd8fddfe4e056966695a920b55ed4f349904195fefabd291794ca8cd351379001c3" +
 			"5a05384a9e85c8c799559833885a188b21f63ab0b8da14e51df431e07fdf";
+
+	private static final String _STEMPEL_URL =
+		_BASE_URL + "org.elasticsearch.plugin.analysis.stempel/7.10.2" +
+			"/org.elasticsearch.plugin.analysis.stempel-7.10.2.zip";
 
 }
