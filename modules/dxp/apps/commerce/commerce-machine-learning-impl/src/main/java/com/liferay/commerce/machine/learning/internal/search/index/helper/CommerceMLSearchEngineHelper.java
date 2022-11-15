4e6aa930cef85caa245aea8829f44285ab799b93
Bryan Engler <bryan.engler@liferay.com>
2022-11-15 10:44:32 -0800

    LPS-166987 LPS-168594 commerce-machine-learning-impl: Dont attempt to create commerce indexes when the search engine is Solr
    https://issues.liferay.com/browse/LPS-168594

--- modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/search/index/helper/CommerceMLSearchEngineHelper.java
+++ modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/search/index/helper/CommerceMLSearchEngineHelper.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.engine.SearchEngineInformation;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.engine.adapter.index.CreateIndexRequest;
 import com.liferay.portal.search.engine.adapter.index.DeleteIndexRequest;
@@ -29,6 +30,8 @@ import com.liferay.portal.search.engine.adapter.index.IndicesExistsIndexResponse
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import java.util.Objects;
+
 /**
  * @author Riccardo Ferrari
  */
@@ -36,6 +39,12 @@ import org.osgi.service.component.annotations.Reference;
 public class CommerceMLSearchEngineHelper {
 
 	public void createIndex(String indexName, String indexMappingFileName) {
+		if (Objects.equals(
+				_searchEngineInformation.getVendorString(), "Solr")) {
+
+			return;
+		}
+
 		if (_indicesExists(indexName)) {
 			if (_log.isDebugEnabled()) {
 				_log.debug(String.format("Index %s already exist", indexName));
@@ -73,6 +82,12 @@ public class CommerceMLSearchEngineHelper {
 	}
 
 	public void dropIndex(String indexName) {
+		if (Objects.equals(
+				_searchEngineInformation.getVendorString(), "Solr")) {
+
+			return;
+		}
+
 		if (!_indicesExists(indexName)) {
 			if (_log.isDebugEnabled()) {
 				_log.debug(String.format("Index %s does not exist", indexName));
@@ -110,5 +125,8 @@ public class CommerceMLSearchEngineHelper {
 
 	@Reference
 	private JSONFactory _jsonFactory;
+
+	@Reference
+	private SearchEngineInformation _searchEngineInformation;
 
 }
