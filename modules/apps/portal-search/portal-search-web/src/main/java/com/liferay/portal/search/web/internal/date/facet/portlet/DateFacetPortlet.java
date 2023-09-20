d73c90bcecd8a7f0715d583e277b47f42e8b6bbf
Bryan Engler <bryan.engler@liferay.com>
2023-09-20 12:01:10 -0700

    LPS-195459 temp workaround

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/date/facet/portlet/DateFacetPortlet.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/date/facet/portlet/DateFacetPortlet.java
@@ -48,7 +48,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-date-facet",
-		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.display-category=category.search",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.icon=/icons/search.png",
 		"com.liferay.portlet.instanceable=true",
