7319f93fdbc563658696a90b234126a81cd84791
gustavolimav <gustavo.lima@liferay.com>
2024-01-11 18:20:25 -0300

    LPS-188842 portal-search-api: Add Constants to each tab in Search Admin

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/constants/SearchAdminConstants.java
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.constants;
+
+/**
+ * @author Gustavo Lima
+ */
+public class SearchAdminConstants {
+
+	public static final String TAB_CONNECTIONS = "connections";
+
+	public static final String TAB_FIELD_MAPPINGS = "field-mappings";
+
+	public static final String TAB_INDEX_ACTIONS = "index-actions";
+
+}
