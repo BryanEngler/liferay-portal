468dc2c05b858f30ddf894d0c94149e4efa564f3
Bryan Engler <bryan.engler@liferay.com>
2026-02-10 10:43:54 -0800

    Revert "LPD-78406 Delete the portal-search-elasticsearch7 directory"
    This reverts commit cacfcf9cacf2bc47a1929e75e790d1a627bc560e.

---
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/ElasticsearchEngineAdapterFixture.java
@@ -0,0 +1,106 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter;
+
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.search.elasticsearch7.internal.connection.ElasticsearchClientResolver;
+import com.liferay.portal.search.elasticsearch7.internal.facet.FacetProcessor;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.cluster.ClusterRequestExecutorTestUtil;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.document.DocumentRequestExecutorFixture;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index.IndexRequestExecutorTestUtil;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.search.SearchRequestExecutorFixture;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.snapshot.SnapshotRequestExecutorTestUtil;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+
+/**
+ * @author Michael C. Han
+ */
+public class ElasticsearchEngineAdapterFixture {
+
+	public SearchEngineAdapter getSearchEngineAdapter() {
+		return _searchEngineAdapter;
+	}
+
+	public void setUp() {
+		_searchEngineAdapter = createSearchEngineAdapter(
+			_elasticsearchClientResolver, _facetProcessor);
+	}
+
+	public void tearDown() {
+		_searchRequestExecutorFixture.tearDown();
+	}
+
+	protected static SearchEngineAdapter createSearchEngineAdapter(
+		ElasticsearchClientResolver elasticsearchClientResolver,
+		FacetProcessor<?> facetProcessor) {
+
+		DocumentRequestExecutorFixture documentRequestExecutorFixture =
+			new DocumentRequestExecutorFixture() {
+				{
+					setElasticsearchClientResolver(elasticsearchClientResolver);
+				}
+			};
+
+		_searchRequestExecutorFixture = new SearchRequestExecutorFixture() {
+			{
+				setElasticsearchClientResolver(elasticsearchClientResolver);
+				setFacetProcessor(facetProcessor);
+			}
+		};
+
+		documentRequestExecutorFixture.setUp();
+		_searchRequestExecutorFixture.setUp();
+
+		SearchEngineAdapter searchEngineAdapter =
+			new ElasticsearchSearchEngineAdapterImpl() {
+				{
+					setThrowOriginalExceptions(true);
+				}
+			};
+
+		ReflectionTestUtil.setFieldValue(
+			searchEngineAdapter, "_clusterRequestExecutor",
+			ClusterRequestExecutorTestUtil.createClusterRequestExecutor(
+				elasticsearchClientResolver));
+		ReflectionTestUtil.setFieldValue(
+			searchEngineAdapter, "_documentRequestExecutor",
+			documentRequestExecutorFixture.getDocumentRequestExecutor());
+		ReflectionTestUtil.setFieldValue(
+			searchEngineAdapter, "_indexRequestExecutor",
+			IndexRequestExecutorTestUtil.createIndexRequestExecutor(
+				elasticsearchClientResolver));
+		ReflectionTestUtil.setFieldValue(
+			searchEngineAdapter, "_searchRequestExecutor",
+			_searchRequestExecutorFixture.getSearchRequestExecutor());
+		ReflectionTestUtil.setFieldValue(
+			searchEngineAdapter, "_snapshotRequestExecutor",
+			SnapshotRequestExecutorTestUtil.createSnapshotRequestExecutor(
+				elasticsearchClientResolver));
+
+		return searchEngineAdapter;
+	}
+
+	protected void setElasticsearchClientResolver(
+		ElasticsearchClientResolver elasticsearchClientResolver) {
+
+		_elasticsearchClientResolver = elasticsearchClientResolver;
+	}
+
+	protected void setFacetProcessor(
+		FacetProcessor<SearchRequestBuilder> facetProcessor) {
+
+		_facetProcessor = facetProcessor;
+	}
+
+	private static SearchRequestExecutorFixture _searchRequestExecutorFixture;
+
+	private ElasticsearchClientResolver _elasticsearchClientResolver;
+	private FacetProcessor<SearchRequestBuilder> _facetProcessor;
+	private SearchEngineAdapter _searchEngineAdapter;
+
+}
