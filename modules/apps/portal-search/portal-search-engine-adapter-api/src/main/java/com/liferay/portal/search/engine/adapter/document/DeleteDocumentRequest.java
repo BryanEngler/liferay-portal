0c67fe8533a42b4719b058dac1d7f7105fc23244
Bryan Engler <bryan.engler@liferay.com>
2026-03-12 12:13:28 -0700

    debug logs

--- modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/document/DeleteDocumentRequest.java
+++ modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/document/DeleteDocumentRequest.java
@@ -5,6 +5,11 @@
 
 package com.liferay.portal.search.engine.adapter.document;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.search.engine.adapter.ccr.CrossClusterRequest;
 
 /**
@@ -17,6 +22,15 @@ public class DeleteDocumentRequest
 	public DeleteDocumentRequest(String indexName, String uid) {
 		_indexName = indexName;
 		_uid = uid;
+
+		if (ArrayUtil.contains(_UIDS, uid)) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					StringBundler.concat(
+						"Deleting ", uid, StringPool.NEW_LINE,
+						_getStackTraceString()));
+			}
+		}
 	}
 
 	@Override
@@ -57,6 +71,46 @@ public class DeleteDocumentRequest
 	public void setType(String type) {
 		_type = type;
 	}
+
+	private String _getStackTraceString() {
+		StringBundler sb = new StringBundler(100);
+
+		Thread thread = Thread.currentThread();
+
+		for (StackTraceElement stackTraceElement : thread.getStackTrace()) {
+			sb.append(stackTraceElement.toString());
+			sb.append(" @@ ");
+
+			if (sb.index() == 100) {
+				break;
+			}
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		sb.setStringAt(StringPool.BLANK, 0);
+		sb.setStringAt(StringPool.BLANK, 1);
+		sb.setStringAt(StringPool.BLANK, 2);
+		sb.setStringAt(StringPool.BLANK, 3);
+
+		return sb.toString();
+	}
+
+	private static final String[] _UIDS = {
+		"com.liferay.document.library.kernel.model." +
+			"DLFileEntry_PORTLET_10269722",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_9394323",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997372",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997358",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997344",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997329",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997311",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997301",
+		"com.liferay.document.library.kernel.model.DLFileEntry_PORTLET_8997287"
+	};
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DeleteDocumentRequest.class);
 
 	private final String _indexName;
 	private boolean _refresh;
